b7b84094116205290a17e5a778b2edd7834cf3fb
Ivan Nikolić <niksy5@gmail.com>
2019-03-27 09:25:57 +0100

    Reorganize BrowserStack configuration

--- karma.conf.js
+++ karma.conf.js
@@ -21,10 +21,10 @@ if ( local ) {
 	};
 } else {
 	config = {
-		username: process.env.BROWSER_STACK_USERNAME,
-		accessKey: process.env.BROWSER_STACK_ACCESS_KEY,
 		hostname: 'bs-local.com',
 		browserStack: {
+			username: process.env.BROWSER_STACK_USERNAME,
+			accessKey: process.env.BROWSER_STACK_ACCESS_KEY,
 			startTunnel: true,
 			project: 'domelo',
 			name: 'Automated (Karma)',
