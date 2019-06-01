e00005adb3e777f3fbe6aa6f3d35ea7921c2da6e
Ivan Nikolić <niksy5@gmail.com>
2019-06-01 18:42:09 +0200

    Reorganize tests

--- test/index.js
+++ test/index.js
@@ -9,184 +9,213 @@ after(function() {
 	window.fixture.cleanup();
 });
 
-it('should return DOM element when using function', function() {
+it('should return DOM element', function() {
 	/* eslint-disable prettier/prettier */
-	const node1 = fn('<div class="becky" data-cali="elvis">gizmo</div>');
-	const node2 = fn('<td id="rocky" data-molly="cleo" louie="coco">sassy</td>');
-	const node3 = fn('<option value="josie">penny</td>');
-	const node4 = fn('<legend id="henry"><span><b>cash</b></span></legend>');
-	const node5 = fn('sydney');
-	const node6 = () => fn('<div>katie</div><p>athena</p>');
-	const node7 = fn(`
-		<div class="becky" data-cali="elvis">${'gizmo'}</div>
-	`);
+	const functionNode = fn('<div class="becky" data-cali="elvis">gizmo</div>');
+	const templateNode = html`<div class="becky" data-cali="elvis">gizmo</div>`;
 	/* eslint-enable */
 
-	const selector1 = 'div.becky[data-cali="elvis"]';
-	const content1 = 'gizmo';
+	const selector = 'div.becky[data-cali="elvis"]';
+	const content = 'gizmo';
+
 	assert.ok(
-		node1.matches(selector1),
-		`Element doesn’t match selector ${selector1}`
+		functionNode.matches(selector),
+		`Element doesn’t match selector ${selector}`
 	);
 	assert.equal(
-		node1.textContent.trim(),
-		content1,
-		`Element doesn’t have content "${content1}"`
+		functionNode.textContent.trim(),
+		content,
+		`Element doesn’t have content "${content}"`
 	);
 
-	const selector2 = 'td#rocky[louie="coco"]';
-	const content2 = 'sassy';
 	assert.ok(
-		node2.matches(selector2),
-		`Element doesn’t match selector ${selector2}`
+		templateNode.matches(selector),
+		`Element doesn’t match selector ${selector}`
 	);
 	assert.equal(
-		node2.textContent.trim(),
-		content2,
-		`Element doesn’t have content "${content2}"`
+		templateNode.textContent.trim(),
+		content,
+		`Element doesn’t have content "${content}"`
 	);
 
-	const selector3 = 'option[value="josie"]';
-	const content3 = 'penny';
-	assert.ok(
-		node3.matches(selector3),
-		`Element doesn’t match selector ${selector3}`
-	);
-	assert.equal(
-		node3.textContent.trim(),
-		content3,
-		`Element doesn’t have content "${content3}"`
-	);
-
-	const selector4 = 'legend#henry';
-	const content4 = '<span><b>cash</b></span>';
-	assert.ok(
-		node4.matches(selector4),
-		`Element doesn’t match selector ${selector4}`
-	);
-	assert.equal(
-		node4.innerHTML.trim(),
-		content4,
-		`Element doesn’t have HTML "${content4}"`
-	);
-
-	const content5 = 'sydney';
-	assert.ok(node5.nodeType === Node.TEXT_NODE, 'Element is not text node');
-	assert.equal(
-		node5.textContent.trim(),
-		content5,
-		`Element doesn’t have content "${content5}"`
-	);
-
-	assert.throws(() => node6(), /Only one root element is allowed/);
-
-	const selector7 = 'div.becky[data-cali="elvis"]';
-	const content7 = 'gizmo';
-	assert.ok(
-		node7.matches(selector7),
-		`Element doesn’t match selector ${selector7}`
-	);
-	assert.equal(
-		node7.textContent.trim(),
-		content7,
-		`Element doesn’t have content "${content7}"`
-	);
-
-	node1.remove();
-	node2.remove();
-	node3.remove();
-	node4.remove();
-	node5.remove();
-	node7.remove();
+	functionNode.remove();
+	templateNode.remove();
 });
 
-it('should return DOM element when using tagged templates', function() {
+it('should return table cell element', function() {
 	/* eslint-disable prettier/prettier */
-	const node1 = html`<div class="becky" data-cali="elvis">gizmo</div>`;
-	const node2 = html`<td id="rocky" data-molly="cleo" louie="coco">sassy</td>`;
-	const node3 = html`<option value="josie">penny</td>`;
-	const node4 = html`<legend id="henry"><span><b>cash</b></span></legend>`;
-	const node5 = html`sydney`;
-	const node6 = () => html`<div>katie</div><p>athena</p>`;
-	const node7 = html`
+	const functionNode = fn('<td id="rocky" data-molly="cleo" louie="coco">sassy</td>');
+	const templateNode = html`<td id="rocky" data-molly="cleo" louie="coco">sassy</td>`;
+	/* eslint-enable */
+
+	const selector = 'td#rocky[louie="coco"]';
+	const content = 'sassy';
+
+	assert.ok(
+		functionNode.matches(selector),
+		`Element doesn’t match selector ${selector}`
+	);
+	assert.equal(
+		functionNode.textContent.trim(),
+		content,
+		`Element doesn’t have content "${content}"`
+	);
+
+	assert.ok(
+		templateNode.matches(selector),
+		`Element doesn’t match selector ${selector}`
+	);
+	assert.equal(
+		templateNode.textContent.trim(),
+		content,
+		`Element doesn’t have content "${content}"`
+	);
+
+	functionNode.remove();
+	templateNode.remove();
+});
+
+it('should return option element', function() {
+	/* eslint-disable prettier/prettier */
+	const functionNode = fn('<option value="josie">penny</option>');
+	const templateNode = html`<option value="josie">penny</option>`;
+	/* eslint-enable */
+
+	const selector = 'option[value="josie"]';
+	const content = 'penny';
+
+	assert.ok(
+		functionNode.matches(selector),
+		`Element doesn’t match selector ${selector}`
+	);
+	assert.equal(
+		functionNode.textContent.trim(),
+		content,
+		`Element doesn’t have content "${content}"`
+	);
+
+	assert.ok(
+		templateNode.matches(selector),
+		`Element doesn’t match selector ${selector}`
+	);
+	assert.equal(
+		templateNode.textContent.trim(),
+		content,
+		`Element doesn’t have content "${content}"`
+	);
+
+	functionNode.remove();
+	templateNode.remove();
+});
+
+it('should return DOM element with multiple children DOM elements', function() {
+	/* eslint-disable prettier/prettier */
+	const functionNode = fn('<legend id="henry"><span><b>cash</b></span></legend>');
+	const templateNode = html`<legend id="henry"><span><b>cash</b></span></legend>`;
+	/* eslint-enable */
+
+	const selector = 'legend#henry';
+	const content = '<span><b>cash</b></span>';
+
+	assert.ok(
+		functionNode.matches(selector),
+		`Element doesn’t match selector ${selector}`
+	);
+	assert.equal(
+		functionNode.innerHTML.trim(),
+		content,
+		`Element doesn’t have HTML "${content}"`
+	);
+
+	assert.ok(
+		templateNode.matches(selector),
+		`Element doesn’t match selector ${selector}`
+	);
+	assert.equal(
+		templateNode.innerHTML.trim(),
+		content,
+		`Element doesn’t have HTML "${content}"`
+	);
+
+	functionNode.remove();
+	templateNode.remove();
+});
+
+it('should return text content as root node', function() {
+	/* eslint-disable prettier/prettier */
+	const functionNode = fn('sydney');
+	const templateNode = html`sydney`;
+	/* eslint-enable */
+
+	const content = 'sydney';
+
+	assert.ok(
+		functionNode.nodeType === Node.TEXT_NODE,
+		'Element is not text node'
+	);
+	assert.equal(
+		functionNode.textContent.trim(),
+		content,
+		`Element doesn’t have content "${content}"`
+	);
+
+	assert.ok(
+		templateNode.nodeType === Node.TEXT_NODE,
+		'Element is not text node'
+	);
+	assert.equal(
+		templateNode.textContent.trim(),
+		content,
+		`Element doesn’t have content "${content}"`
+	);
+
+	functionNode.remove();
+	templateNode.remove();
+});
+
+it('should throw if multiple root elements are provided', function() {
+	/* eslint-disable prettier/prettier */
+	const functioNode = () => fn('<div>katie</div><p>athena</p>');
+	const templateNode = () => html`<div>katie</div><p>athena</p>`;
+	/* eslint-enable */
+
+	assert.throws(() => functioNode(), /Only one root element is allowed/);
+	assert.throws(() => templateNode(), /Only one root element is allowed/);
+});
+
+it('should return DOM element when using spaces at start and end', function() {
+	/* eslint-disable prettier/prettier */
+	const functionNode = fn(`
+		<div class="becky" data-cali="elvis">${'gizmo'}</div>
+	`);
+	const templateNode = html`
 		<div class="becky" data-cali="elvis">${'gizmo'}</div>
 	`;
 	/* eslint-enable */
 
-	const selector1 = 'div.becky[data-cali="elvis"]';
-	const content1 = 'gizmo';
+	const selector = 'div.becky[data-cali="elvis"]';
+	const content = 'gizmo';
+
 	assert.ok(
-		node1.matches(selector1),
-		`Element doesn’t match selector ${selector1}`
+		functionNode.matches(selector),
+		`Element doesn’t match selector ${selector}`
 	);
 	assert.equal(
-		node1.textContent.trim(),
-		content1,
-		`Element doesn’t have content "${content1}"`
+		functionNode.textContent.trim(),
+		content,
+		`Element doesn’t have content "${content}"`
 	);
 
-	const selector2 = 'td#rocky[louie="coco"]';
-	const content2 = 'sassy';
 	assert.ok(
-		node2.matches(selector2),
-		`Element doesn’t match selector ${selector2}`
+		templateNode.matches(selector),
+		`Element doesn’t match selector ${selector}`
 	);
 	assert.equal(
-		node2.textContent.trim(),
-		content2,
-		`Element doesn’t have content "${content2}"`
+		templateNode.textContent.trim(),
+		content,
+		`Element doesn’t have content "${content}"`
 	);
 
-	const selector3 = 'option[value="josie"]';
-	const content3 = 'penny';
-	assert.ok(
-		node3.matches(selector3),
-		`Element doesn’t match selector ${selector3}`
-	);
-	assert.equal(
-		node3.textContent.trim(),
-		content3,
-		`Element doesn’t have content "${content3}"`
-	);
-
-	const selector4 = 'legend#henry';
-	const content4 = '<span><b>cash</b></span>';
-	assert.ok(
-		node4.matches(selector4),
-		`Element doesn’t match selector ${selector4}`
-	);
-	assert.equal(
-		node4.innerHTML.trim(),
-		content4,
-		`Element doesn’t have HTML "${content4}"`
-	);
-
-	const content5 = 'sydney';
-	assert.ok(node5.nodeType === Node.TEXT_NODE, 'Element is not text node');
-	assert.equal(
-		node5.textContent.trim(),
-		content5,
-		`Element doesn’t have content "${content5}"`
-	);
-
-	assert.throws(() => node6(), /Only one root element is allowed/);
-
-	const selector7 = 'div.becky[data-cali="elvis"]';
-	const content7 = 'gizmo';
-	assert.ok(
-		node7.matches(selector7),
-		`Element doesn’t match selector ${selector7}`
-	);
-	assert.equal(
-		node7.textContent.trim(),
-		content7,
-		`Element doesn’t have content "${content7}"`
-	);
-
-	node1.remove();
-	node2.remove();
-	node3.remove();
-	node4.remove();
-	node5.remove();
-	node7.remove();
+	functionNode.remove();
+	templateNode.remove();
 });
